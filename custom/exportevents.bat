eacb687b96c5cf1e5f622ba3acaa389dc30c01f3
Mat Davis <mdavis5@us.ibm.com>
2019-05-13 10:50:09 -0400

    Smart export events script

---
+++ custom/exportevents.bat
@@ -0,0 +1,22 @@
+@REM *************** Begin Standard Header - Do not add comments here ***************
+@REM 
+@REM File:     %W%
+@REM Version:  %I%
+@REM Modified: %G% %U%
+@REM Build:    %R% %L%
+@REM 
+@REM Licensed Materials - Property of IBM
+@REM 
+@REM Restricted Materials of IBM
+@REM 
+@REM 5724-N55
+@REM 
+@REM (C) COPYRIGHT IBM CORP. 2007.  All Rights Reserved.
+@REM 
+@REM US Government Users Restricted Rights - Use, duplication or
+@REM disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+@REM 
+@REM **************************** End Standard Header *******************************
+
+
+@call ..\bin\jython_coll.bat exportevents.jy %*
