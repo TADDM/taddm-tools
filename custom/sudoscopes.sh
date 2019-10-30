fe52e3f8e151ecf4accb139ad982246974e0455c
doormat18 <mat.davis@gmail.com>
2019-10-30 08:52:10 -0400

    Add 7 day timeframe

--- custom/sudoscopes.sh
+++ custom/sudoscopes.sh
@@ -13,17 +13,22 @@ BINDIR=$COLLATION_HOME/bin
 COMMONPART="$BINDIR/common.sh"
 . $COMMONPART
 
+# calculate epoch for 7 days ago
+day=$((60*60*24))
+now=`date +%s`
+epoch=$((($now - 7*$day)*1000))
+
 echo "# sudo_invalid" > scopes/sudo_invalid.scope
-./buildscope.jy -P -q "select * from ComputerSystem where XA eval '/xml[attribute[@name=\"sudo_verified\"]=\"invalid\"]'" >> scopes/sudo_invalid.scope
+./buildscope.jy -P -q "select * from ComputerSystem where XA eval '/xml[attribute[@name=\"sudo_verified\"]=\"invalid\"]' and lastModifiedTime > $epoch" >> scopes/sudo_invalid.scope
 
 echo "# sudo_lsof_invalid" > scopes/sudo_lsof_invalid.scope
-./buildscope.jy -P -q "select * from ComputerSystem where XA eval '/xml[attribute[@name=\"sudo_lsof\"]=\"invalid\"]'" >> scopes/sudo_lsof_invalid.scope
+./buildscope.jy -P -q "select * from ComputerSystem where XA eval '/xml[attribute[@name=\"sudo_lsof\"]=\"invalid\"]' and lastModifiedTime > $epoch" >> scopes/sudo_lsof_invalid.scope
 
 echo "# sudo_dmidecode_invalid" > scopes/sudo_dmidecode_invalid.scope
-./buildscope.jy -P -q "select * from LinuxUnitaryComputerSystem where XA eval '/xml[attribute[@name=\"sudo_dmidecode\"]=\"invalid\"]'" >> scopes/sudo_dmidecode_invalid.scope
+./buildscope.jy -P -q "select * from LinuxUnitaryComputerSystem where XA eval '/xml[attribute[@name=\"sudo_dmidecode\"]=\"invalid\"]' and lastModifiedTime > $epoch" >> scopes/sudo_dmidecode_invalid.scope
 
 echo "# sudo_hba_invalid" > scopes/sudo_hba_invalid.scope
-./buildscope.jy -P -q "select * from ComputerSystem where XA eval '/xml[attribute[@name=\"sudo_hba\"]=\"invalid\"]' and ( virtual is-null or not virtual )" >> scopes/sudo_hba_invalid.scope
+./buildscope.jy -P -q "select * from ComputerSystem where XA eval '/xml[attribute[@name=\"sudo_hba\"]=\"invalid\"]' and ( virtual is-null or not virtual ) and lastModifiedTime > $epoch" >> scopes/sudo_hba_invalid.scope
 
 echo "# sudo_rdm_invalid" > scopes/sudo_rdm_invalid.scope
-./buildscope.jy -P -q "select * from LinuxUnitaryComputerSystem where XA eval '/xml[attribute[@name=\"sudo_rdm\"]=\"invalid\"]'" >> scopes/sudo_rdm_invalid.scope
+./buildscope.jy -P -q "select * from LinuxUnitaryComputerSystem where XA eval '/xml[attribute[@name=\"sudo_rdm\"]=\"invalid\"]' and lastModifiedTime > $epoch" >> scopes/sudo_rdm_invalid.scope
