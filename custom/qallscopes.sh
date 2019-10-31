59d70e9b0a414f0e397dcd6fe83e18755de545e9
Mat Davis <mdavis5@us.ibm.com>
2019-10-31 15:01:21 -0400

    query scopes

---
+++ custom/qallscopes.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BINDIR=`dirname $0`/../bin
+COMMONPART="$BINDIR/common.sh"
+. $COMMONPART
+cd $COLLATION_HOME/custom
+
+mkdir scopes 2> /dev/null
+
+# !!! CHANGE THESE VARIABLES TO APPLY TO YOUR ENVIRONMENT !!!
+USER=administrator
+PASSWORD=collation
+
+../sdk/bin/api.sh -u $USER -p $PASSWORD find -d 1 "select name from Scope" | awk -F'[<>]' '/name/ {print $3}' | while read line
+do 
+    ./queryscopes.jy -u $USER -p $PASSWORD -s "$line" > scopes/`echo $line | tr ' ' _`.scope
+done
