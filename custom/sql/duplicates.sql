536472f27e0cff84c691e7c539511b4a1c9e56bf
doormat18 <mat.davis@gmail.com>
2020-01-03 15:14:03 -0500

    simplify sql

--- custom/sql/duplicates.sql
+++ custom/sql/duplicates.sql
@@ -1,7 +1,10 @@
 SELECT T1.guid_c      AS FIRST_GUID,
-       T2.guid_c      AS SECOND_GUID,
+       T1.LASTMODIFIEDTIME_T AS FIRST_LMT,
+       --T2.guid_c      AS SECOND_GUID,
+       --T2.LASTMODIFIEDTIME_T AS SECOND_LMT,
        T1.signature_c AS FIRST_SIG,
-       T2.signature_c AS SECOND_SIG,
+       --T2.signature_c AS SECOND_SIG,
+       OS1.OSNAME_C AS OSNAME,
        T1.fqdn_c
 FROM   bb_computersystem40_v T1,
        bb_computersystem40_v T2,
