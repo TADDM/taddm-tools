cba0f11071ac0d6aa159cd641f45ed752e4f6da1
doormat18 <mat.davis@gmail.com>
2020-04-21 09:55:35 -0400

    bug

--- custom/sendsudo.sh
+++ custom/sendsudo.sh
@@ -21,32 +21,29 @@ do
   for scope in "${scopes[@]}"
   do
     if [ -f "scopes/${scope}_invalid_${org}.scope" ]; then
-      if [ `wc -l scopes/${scope}_invalid_${org}.scope|awk '{print $1}'` -gt 1 ]; then
+      if [ `wc -l scopes/${scope}_invalid_${org}.scope|awk '{print $1}'` -ge 1 ]; then
         attachments="$attachments -a scopes/${scope}_invalid_${org}.scope"
       fi
     fi
   done
 
   if [[ ! -z "$attachments" ]]; then
+    attachments="$attachments -a sudoers.txt"
     mailx -s "TADDM sudo config (${org})" -r "TADDM <${USER}@${HOSTNAME}>" $attachments $EMAIL 2>/dev/null << EOL
-Attached are scope files containing hosts where sudo is either not configured or not configured properly. Note that if a particular file is not attached, then there were no issues detected for that config type. 
+Attached are text files (.scope) containing hosts where sudo is either not configured or not configured properly. Note that if a particular file is not attached, then there were no issues detected for that config type.
 
 Key:
   sudo_invalid_${org}.scope - sudo not configured or requiretty problem
   sudo_lsof_invalid_${org}.scope - lsof not in sudo
   sudo_dmidecode_invalid_${org}.scope - dmidecode not in sudo for Linux host
   sudo_hba_invalid_${org}.scope - collectionengine not in sudo and/or fcinfo not in sudo for Solaris and/or powermt not in sudo for physical Linux if installed
+  sudo_emc_invalid_${org}.scope - EMC INQ tool not in sudo, also move inq.<platform> binary from service account home directory to /usr/local/bin
   sudo_hba_path_invalid_${org}.scope - collectionengine path mismatch, most likely unexpected home dir path for service account
   sudo_rdm_invalid_${org}.scope - sg_inq not in sudo for VMware Linux VM containing RDM
 
-Below is the combined sudoers configuration for Linux and Solaris:
+Also attached is sudoers.txt, which is the official, approved sudoers configuration for TADDM.
+Use these values to configure sudo for the TADDM service account.
 
-Defaults !requiretty
-(root) NOPASSWD: /usr/sbin/dmidecode, /usr/sbin/lsof, /usr/local/bin/lsof, /opt/VRTSvcs/bin/hastatus,
-    /opt/VRTSvcs/bin/haclus, /opt/VRTSvcs/bin/hasys, /opt/VRTSvcs/bin/hares, /opt/VRTSvcs/bin/hagrp,
-    /opt/VRTSvcs/bin/hatype, /opt/VRTSvcs/bin/hauser, /var/TADDM/home/tadmadm/collectionengine-linux-x86*,
-    /var/TADDM/home/tadmadm/collectionengine-solaris-sparc, /usr/sbin/fcinfo, /usr/bin/sg_inq,
-	/sbin/powermt display dev\=all
 EOL
   fi
 done
