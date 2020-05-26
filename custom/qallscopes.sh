120d2ef273d8af9b2e3ad3bfbddf0b289f837230
doormat18 <mat.davis@gmail.com>
2020-05-26 15:14:32 -0400

    add prefix arg

--- custom/qallscopes.sh
+++ custom/qallscopes.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
 
+#set -x
+
 BINDIR=`dirname $0`/../bin
 COMMONPART="$BINDIR/common.sh"
 . $COMMONPART
 cd $COLLATION_HOME/custom
 
-mkdir scopes 2> /dev/null
-
 # !!! CHANGE THESE VARIABLES TO APPLY TO YOUR ENVIRONMENT !!!
 USER=operator
 PASSWORD=collation
 
-../sdk/bin/api.sh -u $USER -p $PASSWORD find -d 1 "select name from Scope" | awk -F'[<>]' '/name/ {print $3}' | while read line
-do 
+usage() {
+  echo "Usage: $0 { -p <prefix> }"
+  echo "  Use -p to feed in scopeset name prefix to filter results"
+}
+
+while getopts ":p:h" o; do
+  case "${o}" in
+    p)
+      p=${OPTARG}
+      ;;
+    h)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+SQL="select name from Scope"
+if [ ! -z "${p}" ]; then
+  SQL="$SQL where name starts-with '${p}'"
+fi
+
+../sdk/bin/api.sh -u $USER -p $PASSWORD find -d 1 "${SQL}" | awk -F'[<>]' '/name/ {print $3}' | while read line
+do
     ./queryscopes.jy -u $USER -p $PASSWORD -s "$line" > scopes/`echo $line | tr ' ' _`.scope
 done
